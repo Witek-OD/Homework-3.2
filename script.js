a59ee4d029c596410e3c76c0ca88f8f603d9c8d7
Wiktor Stoianov <stojanov87@gmail.com>
2024-05-10 10:12:10 +0300

    string concatenation

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.addEventListener('load', () => {
+
+    let str1= prompt('Enter first string');
+    let str2= prompt('Enter second string');
+    let str3= prompt('Enter last string');
+
+    alert(`Result : ${str1} + ${str2} + ${str3}`);
+
+});
